9bf3601a44b2542d6906e00f2a53a915a0bbccd0
Admin <vuongnguyenbinhh@gmail.com>
2020-02-16 15:56:50 +0700

    them

--- models/NguoiDung.js
+++ models/NguoiDung.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-console.log(abc);
-=======
 var NguoiDung = function(){
     this.TaiKhoa = '';
     this.HoTen = '';
     this.Tuoi = '';
     this.GioiTinh = '';
-<<<<<<< HEAD
-}
->>>>>>> df9b6298341dfa5512e95def68b1ae76d56e0ce8
-=======
     this.QuocTich ='';
 }
->>>>>>> 5ba41e6d570f89e089ddc2554c150c75d8632cc2
